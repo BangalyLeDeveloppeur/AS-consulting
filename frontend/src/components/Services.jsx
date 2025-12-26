427222ece9db010f78ddc3bdc463e44e3794678e
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2025-12-26 19:49:33 +0100

    mise a jour du composant services

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Services = () => {
+  const [services, setServices] = useState([]);
+  console.log(" Voici le contenu de votre ", services);
+
+  const fetchServices = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/api/services");
+      setServices(res.data);
+    } catch (error) {
+      console.log("voici  votre erreur", error);
+    }
+  };
+  useEffect(() => {
+    fetchServices();
+  }, []);
+
   return (
     <div className="service">
       <h1>Nos services</h1>
@@ -8,6 +25,17 @@ const Services = () => {
         Au-delà des chiffres, AS consulting est un cabinet d’expertise comptable
         offrant un service humain et se soucie de votre succès.
       </p>
+      {services.map((service) => (
+        <article>
+
+        <div className="service-container">
+        <div key={service.id}>
+          <h2>{service.titre}</h2>
+          <p>{service.description}</p>
+        </div>
+        </div>
+        </article>
+      ))}
     </div>
   );
 };
