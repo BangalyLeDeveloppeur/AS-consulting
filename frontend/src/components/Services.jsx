232cc44f68edb42fee4a23b8ab4923244fa706c0
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2025-12-27 11:30:04 +0100

    mise a jour dus style de services

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -20,19 +20,21 @@ const Services = () => {
 
   return (
     <div className="service">
+      <div className="service-titre">
       <h1>Nos services</h1>
       <p>
         Au-delà des chiffres, AS consulting est un cabinet d’expertise comptable
         offrant un service humain et se soucie de votre succès.
       </p>
+      </div>
       <div className="container text-center">
   <div className="row row-cols-1 row-cols-md-3 g-4">
     {services.map((service) => (
       <div key={service.id} className="col">
-        <div className="card h-100 border-0 shadow">
+        <div className="card cadre-service">
           <div className="card-body d-flex flex-column">
-            <h2 className="h4 card-title">{service.titre}</h2>
-            <p className="card-text mb-4">{service.description}</p>
+            <h2 className="card-title">{service.titre}</h2>
+            <p className="card-text">{service.description}</p>
             <div className="mt-auto pt-3">
               <button className="btn btn decouvrez">
                 Découvrir <i className="bi bi-chevron-right ms-1"></i>
