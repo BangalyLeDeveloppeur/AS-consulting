1d42c0887e6703a26e7312ceae0d9decc4ab6e29
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2025-12-28 10:29:02 +0100

    mise a jour du composant pourquoiNous

--- frontend/src/components/PourquoiNous.jsx
+++ frontend/src/components/PourquoiNous.jsx
@@ -3,34 +3,34 @@ import femmmeSourientes from "../assets/image/Femmes.png";
 
 const PourquoiNous = () => {
   return (
-    <div className="card">
+    <div className="card cdrNous">
+        <h1>Pourquoi nous choisir</h1>
       <section className="choix">
+        <div className="choix-texte">
+          <h2>
+            Un service comptable et fiscal de qualité
+          </h2>
+          <p>
+            Au-delà des chiffres, AS Consulting est un cabinet d’expertise
+            comptable qui place l’humain au cœur de son accompagnement et se
+            consacre pleinement à la réussite de ses clients.
+          </p>
 
-      <div>
-        <h1>
-          Nous fournissons un service comptable et fiscal de haute qualité
-        </h1>
-        <p>
-          Au-delà des chiffres, AS Consulting est un cabinet d’expertise
-          comptable qui place l’humain au cœur de son accompagnement et se
-          consacre pleinement à la réussite de ses clients.
-        </p>
-
-        <h2>Meilleur service comptable</h2>
-        <p>
-          Libérez-vous des contraintes comptables : notre service de tenue de
-          livres assure une gestion claire, fiable et sans stress
-        </p>
-        <h3>Meilleur service comptable</h3>
-        <p>
-          Nous produisons des documents comptables de haute qualité pour
-          accompagner votre croissance et vos ambitions entrepreneuriales.
-        </p>
-        <button className="btn-nous">Prendre rendez-vous</button>
-      </div>
-      <div>
-        <img src={femmmeSourientes} alt="femme sourinets au bureau" />
-      </div>
+          <h3>Meilleur service comptable</h3>
+          <p>
+            Libérez-vous des contraintes comptables : notre service de tenue de
+            livres assure une gestion claire, fiable et sans stress
+          </p>
+          <h3>Meilleur service comptable</h3>
+          <p>
+            Nous produisons des documents comptables de haute qualité pour
+            accompagner votre croissance et vos ambitions entrepreneuriales.
+          </p>
+          <button className="btn-nous">Prendre rendez-vous</button>
+        </div>
+        <div>
+          <img src={femmmeSourientes} alt="femme sourinets au bureau" />
+        </div>
       </section>
     </div>
   );
