57acfd6625f002347f863ef8c08b087a6b939d58
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2025-12-29 18:36:06 +0100

    mise ajour du backend

---
+++ frontend/src/admin/components/Temoignage.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+////Déclaration de mes states////
+const Temoignages = () => {
+  const [titre, setTitre] = useState("");
+  const [description, setDescription] = useState("");
+  const [message, setMessage] = useState("");
+  /// la gestion de formulaire/////
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    ///me permet d'envoyer image+texte + titre dans ma requet http(uploard)
+    const formData = new FormData();
+    formData.append("titre", titre);
+    formData.append("description", description);
+    console.log(formData);
+
+    try {
+      await axios.post("http://localhost:5000/api/temoignages", formData);
+
+      setMessage(" Temoignage ajouté avec succès !");
+      setTitre("");
+      setDescription("");
+      console.log(formData);
+    } catch (error) {
+      console.error("Erreur lors de l'upload :", error);
+      setMessage(" Erreur lors de l'upload");
+    }
+  };
+
+  return (
+    <div>
+      <section className="admin-section">
+        <h1 className="titre"> Temoignages</h1>
+
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Titre :</label>
+            <input
+              type="text"
+              value={titre}
+              onChange={(e) => setTitre(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>Description :</label>
+            <input
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className="botton">
+            Ajouter
+          </button>
+        </form>
+
+        {message && <p>{message}</p>}
+
+        <h3>Services enregistrées :</h3>
+      </section>
+    </div>
+  );
+};
+
+export default Temoignages;
--- frontend/src/admin/pagesAdmin/AdminHome.jsx
+++ frontend/src/admin/pagesAdmin/AdminHome.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import AdSlide from '../components/adSlide';
 import Services from '../components/Services';
+import Temoignage from '../components/Temoignage';
 
 const AdminHome = () => {
     return (
         <div>
             <AdSlide/>
             <Services />
+            <Temoignage />
         </div>
     );
 };
