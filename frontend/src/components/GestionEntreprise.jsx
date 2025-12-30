74ea1f2965565ada0032f2ea605b6bce148f31e5
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2025-12-30 10:06:24 +0100

    style gestion entreprise

--- frontend/src/components/GestionEntreprise.jsx
+++ frontend/src/components/GestionEntreprise.jsx
@@ -3,17 +3,22 @@ import Brochure from "../assets/image/Brochure.png";
 
 const GestionEntreprise = () => {
   return (
-    <div>
-      <h1>Le bon statut, les bonnes décisions, le bon départ</h1>
+    <div className="gb">
+      <div className="cadreParagraphe comp">
+        <h1>Le bon statut, les bonnes décisions, le bon départ</h1>
+      </div>
       <img src={Brochure} alt="livre" />
-      <p>
-        AS-Consulting vous accompagne dès la création de votre entreprise en
-        simplifiant le choix du statut juridique et les formalités
-        administratives. Grâce à notre expertise, vous démarrez rapidement,
-        évitez les erreurs coûteuses et lancez votre activité sur des bases
-        solides.
-      </p>
-      <button className="btn-nous">Prendre rendez-vous</button>
+      <h2>Création est gestion d’entreprise</h2>
+      <div className="creation-entreprise">
+        <p>
+          AS-Consulting vous accompagne dès la création de votre entreprise en
+          simplifiant le choix du statut juridique et les formalités
+          administratives. Grâce à notre expertise, vous démarrez rapidement,
+          évitez les erreurs coûteuses et lancez votre activité sur des bases
+          solides.
+        </p>
+      </div>
+      <button className="btn-nous bt">Prendre rendez-vous</button>
     </div>
   );
 };
