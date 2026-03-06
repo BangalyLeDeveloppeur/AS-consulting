e0ae867ac0d47e74825ef8e3a88dda272e89d9e3
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2026-03-06 11:11:03 +0100

    mise en jour login

--- frontend/src/admin/components/Login.jsx
+++ frontend/src/admin/components/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/admin",
+        "http://localhost:5000/api/admin/login",
         loginData,
         {
           headers: {
